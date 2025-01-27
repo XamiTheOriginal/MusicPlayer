4d35678a1774545ae42613c9b770f5553e7b4315
XamiTheOriginal <122601380+XamiTheOriginal@users.noreply.github.com>
2025-01-27 12:32:43 +0530

    added a class, need to code it for the codex

---
+++ Player/Converter.cs
@@ -0,0 +1,31 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    public class Converter
+    {
+        WaveFileWriter writer;
+        string filepath;
+
+        public Converter(WaveFileWriter writer, string filepath)
+        {
+            this.writer = writer;
+            this.filepath = filepath;
+        }
+
+        public void toWave()
+        {
+            //TODO : A implem
+        }
+
+        public void toMP3()
+        {
+            //TODO : A implem
+        }
+    }
+}
