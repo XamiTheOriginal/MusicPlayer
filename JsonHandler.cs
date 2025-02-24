6a3a1220b98f9acd5b88c6804e4a15d9942a63fa
theobaldmazet <thblink1995@gmail.com>
2025-02-24 10:48:35 +0530

    réorganisation des fichiers

---
+++ JsonHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MusicPlayer
+{
+    public class JsonHandler
+    {
+        private readonly string _filePath;
+
+        public JsonHandler(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        // Sauvegarde une liste avec un nom (sans écraser les autres)
+        public void SaveToJson(string name, List<int> numbers)
+        {
+            Dictionary<string, List<int>> data = LoadAll();
+
+            data[name] = numbers; // Ajoute ou met à jour la liste
+
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
+
+        // Charge une liste spécifique
+        public List<int> LoadFromJson(string name)
+        {
+            Dictionary<string, List<int>> data = LoadAll();
+
+            return data.TryGetValue(name, out var numbers) ? numbers : new List<int>();
+        }
+
+        // Charge toutes les données du fichier (ou crée un dictionnaire vide)
+        private Dictionary<string, List<int>> LoadAll()
+        {
+            if (!File.Exists(_filePath))
+                return new Dictionary<string, List<int>>();
+
+            string json = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<Dictionary<string, List<int>>>(json) ??
+                   new Dictionary<string, List<int>>();
+        }
+    }
+}
---
+++ Playlist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MusicPlayer
+{
+    public class Playlist
+    {
+        public List<int> SongList { get; set; }
+        public string Name { get; set; }
+
+        public Playlist(List<int> list, string name)
+        {
+            SongList = list ?? new List<int>();
+            Name = name;
+        }
+
+        public void Add(int item)
+        {
+            SongList.Add(item);
+        }
+
+        public void Remove(int item)
+        {
+            SongList.Remove(item);
+        }
+
+
+       
+    }
+}
