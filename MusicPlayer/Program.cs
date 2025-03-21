e2810689fe5c46b65fd1acc553f8db4ce6161425
Thblink1995 <145861430+Thblink1995@users.noreply.github.com>
2025-03-21 18:35:22 +0530

    tkt

--- MusicPlayer/Program.cs
+++ MusicPlayer/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Avalonia;
 using System;
+using MusicPlayer.SongsHandler;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MusicPlayer.SongsHandler.Managers;
 
 namespace MusicPlayer;
 
@@ -9,9 +13,28 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+        
+        var services = new ServiceCollection();
+        services.AddMusicManagers(); // Ajout des managers au conteneur DI
 
+        services.AddSingleton<Player>(); //Ajoute le singleton du player
+        
+        var provider = services.BuildServiceProvider();
+        ServiceLocator.Init(provider);
+        var songsManager = provider.GetRequiredService<SongsManager>();
+        var playlistsManager = provider.GetRequiredService<PlaylistsManager>();
+    }
+    
+    /* Pour récupérer songsManager et playlistsManager ailleurs :
+    using Microsoft.Extensions.DependencyInjection;
+    var playlistsManager = ServiceLocator.Instance.GetRequiredService<PlaylistsManager>();
+    var songsManager =  ServiceLocator.Instance.GetRequiredService<SongsManager>();
+    */
+    
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
