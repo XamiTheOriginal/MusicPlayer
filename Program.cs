61abe566b52defa258af27bf986c57f9bba04608
XamiTheOriginal <122601380+XamiTheOriginal@users.noreply.github.com>
2025-04-07 10:37:09 +0530

    modif du player

--- Program.cs
+++ Program.cs
@@ -7,33 +7,23 @@ namespace MusicPlayer;
 
 class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
+        // D'abord on prépare la DI
+        var services = new ServiceCollection();
+
+        services.AddMusicManagers(); // Ajout des managers
+        services.AddSingleton<Player>(); // Singleton du player
+
+        var provider = services.BuildServiceProvider();
+        ServiceLocator.Init(provider); // Initialisation du ServiceLocator avant tout
+
+        // Maintenant on lance Avalonia (le DI est déjà prêt)
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
-        
-        var services = new ServiceCollection();
-        services.AddMusicManagers(); // Ajout des managers au conteneur DI
-
-        services.AddSingleton<Player>(); //Ajoute le singleton du player
-        
-        var provider = services.BuildServiceProvider();
-        ServiceLocator.Init(provider);
-        var songsManager = provider.GetRequiredService<SongsManager>();
-        var playlistsManager = provider.GetRequiredService<PlaylistsManager>();
     }
-    
-    /* Pour récupérer songsManager et playlistsManager ailleurs :
-    using Microsoft.Extensions.DependencyInjection;
-    var playlistsManager = ServiceLocator.Instance.GetRequiredService<PlaylistsManager>();
-    var songsManager =  ServiceLocator.Instance.GetRequiredService<SongsManager>();
-    */
-    
-    // Avalonia configuration, don't remove; also used by visual designer.
+
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
